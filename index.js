75d525b1c1ddf4a613d8fcf711b5589f7d48a80e
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2020-07-13 13:19:09 +0700

    init project

---
+++ index.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const puppeteer = require("puppeteer");
+const moment = require("moment");
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: process.env.HEADLESS === 'true' || false,
+    defaultViewport: null,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+  await page.goto("https://portal.polinema.ac.id", {
+    waitUntil: "networkidle2",
+  });
+  await page.type("#username", process.env.USERNAME);
+  await page.waitFor(3000);
+  await page.type("#password", process.env.PASSWORD);
+  await page.waitFor(3000);
+  await page.click(".submit");
+  await page.waitForNavigation();
+  await page.click("a[href$='#tab_presensi']");
+  await page.waitFor(3000);
+  const [button] = await page.$x("//button[contains(., ' Presensi Online')]");
+  await page.waitFor(3000);
+  if (button) {
+    await button.click({
+      waitUntil: "networkidle2",
+    });
+  }
+  await page.waitForSelector("#table_data-fp_online");
+  const [buttonAbsen] = await page.$x("//button[contains(., 'KLIK UNTUK')]");
+  if (buttonAbsen) {
+    await buttonAbsen.click({
+      waitUntil: "networkidle2",
+    });
+  }
+  const date = new Date();
+  const formattedDate = moment(date).format("YYYYMMDD");
+  await page.waitFor(3000);
+  await page.screenshot({ path: formattedDate + ".png" });
+  await browser.close();
+})();
